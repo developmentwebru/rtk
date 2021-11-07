7505ecb5fe5c486d6dfd509a76f6fecdee881a4f
sergeisaz <almagroup2019@gmail.com>
2021-11-07 16:57:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TodoList from "./components/TodoList";
+import InputField from "./components/InputField";
+import "./App.css";
+import { addNewTodo, fetchTodos } from "./store/todoSlice";
 
 function App() {
+  const [text, setText] = useState("");
+  const { status, error } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  const addTask = () => {
+    dispatch(addNewTodo( text ));
+    setText("");
+  };
+
+  const toggleTodoComplete = (todoId) => {
+    // setTodos(
+    //   todos.map((todo) => {
+    //     if (todo.id !== todoId) return todo;
+    //     return {
+    //       ...todo,
+    //       completed: !todo.completed,
+    //     };
+    //   })
+    // );
+  };
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputField text={text} handleInput={setText} handleSubmit={addTask} />
+      {status === "loading" && <h2>Loading...</h2>}
+      {error && <h2>An error occurred: {error}</h2>}
+      <TodoList />
     </div>
   );
 }
